e97920d5eff93669a4938e5e33fac1600691758b
Khaled Zakaria <khaled71612000@gmail.com>
2022-06-30 15:02:48 +0200

    kz

---
+++ Source/Arkanoid/ArkanoidGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArkanoidGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARKANOID_API AArkanoidGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
